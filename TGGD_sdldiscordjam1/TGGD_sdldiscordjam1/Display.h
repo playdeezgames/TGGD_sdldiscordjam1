07767841595449944fdb492b51a708158225a1d4
ernestpazera <ernestpazera@gmail.com>
2022-01-30 05:59:55 -0600

    moved all of the stuff into Display class

---
+++ TGGD_sdldiscordjam1/TGGD_sdldiscordjam1/Display.h
@@ -0,0 +1,6 @@
+#pragma once
+struct Display
+{
+	Display() = delete;
+	static void Run();
+};
--- TGGD_sdldiscordjam1/TGGD_sdldiscordjam1/TGGD_sdldiscordjam1.cpp
+++ TGGD_sdldiscordjam1/TGGD_sdldiscordjam1/TGGD_sdldiscordjam1.cpp
@@ -1,53 +1,8 @@
 #include <SDL.h>
-#include <SDL_image.h>
+#include "Display.h"
 
 int main(int argc, char** argv)
 {
-	constexpr auto SCREEN_WIDTH = 1280;
-	constexpr auto SCREEN_HEIGHT = 720;
-	constexpr auto LOGICAL_WIDTH = 640;
-	constexpr auto LOGICAL_HEIGHT = 360;
-
-	SDL_Init(SDL_INIT_EVERYTHING);
-	IMG_Init(IMG_INIT_PNG);
-
-	SDL_Window* window = nullptr;
-	SDL_Renderer* renderer = nullptr;
-
-	SDL_CreateWindowAndRenderer(SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_RESIZABLE, &window, &renderer);
-	SDL_SetWindowTitle(window, "TGGD_sdldiscordjam1");
-	SDL_RenderSetLogicalSize(renderer, LOGICAL_WIDTH, LOGICAL_HEIGHT);
-
-	SDL_Texture* texture = IMG_LoadTexture(renderer, "romfont8x8.png");
-
-	SDL_Event evt{};
-
-	bool done = false;
-	while (!done)
-	{
-		while (SDL_PollEvent(&evt))
-		{
-			done = evt.type == SDL_QUIT;
-		}
-		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-		SDL_RenderClear(renderer);
-		SDL_RenderPresent(renderer);
-	}
-
-	if (texture)
-	{
-		SDL_DestroyTexture(texture);
-	}
-	if (window)
-	{
-		SDL_DestroyWindow(window);
-	}
-	if (renderer)
-	{
-		SDL_DestroyRenderer(renderer);
-	}
-
-	IMG_Quit();
-	SDL_Quit();
+	Display::Run();
 	return 0;
 }
